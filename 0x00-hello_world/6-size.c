07c32dba3668c6c82138795bd8294e2265ad22f8
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-09-09 09:02:32 -0700

    C program

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,26 +1,22 @@
 #include <stdio.h>
 
-/**
-*
+/***
 * main - Prints C types
 *
 * Return: Always 0 (Success)
 */
 int main(void)
 {
-
-	int intType;
-	float floatType;
+	char a;
+	int b;
 	long int longintType;
 	long long int longlongintType;
-	char charType;
+	float c;
 
-	printf("Size of int: %zu byte(s)\n", sizeof(intType));
-	printf("Size of float: %zu byte(s)\n", sizeof(floatType));
-	printf("Size of long int: %zu byte(s)\n", sizeof(longintType));
-	printf("Size of long long int: %zu byte(s)\n", sizeof(longlongintType));
-	printf("Size of char: %zu byte(s)\n", sizeof(charType));
-
+	printf("Size of a char: %zu byte(s)\n", sizeof(a));
+	printf("Size of an int: %zu byte(s)\n", sizeof(b));
+	printf("Size of a long int: %zu byte(s)\n", sizeof(longintType));
+	printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongintType));
+	printf("Size of a float: %zu byte(s)\n", sizeof(c));
 	return (0);
-
 }
