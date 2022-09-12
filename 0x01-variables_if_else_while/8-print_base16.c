d16b0f23cd4bd5ab05d76e0324ef71d49e54128e
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-09-12 13:10:47 -0700

    Variables

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main -  prints all the numbers of base 16 in lowercase.
+ *
+ * Return: (0)
+ */
+
+int main(void)
+
+{
+	char num, alpha;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+	}
+	for (alpha = 'a'; alpha <= f; alpha++)
+	{
+		putchar(alpha);
+	}
+	putchar('\n');
+
+	return (0);
+{
