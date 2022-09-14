b55a90c3c569c71ef524939e2501a6172ccf2105
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-09-14 12:23:05 -0700

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - print _putchar
+ *
+ * Return: always 0
+ */
+
+int maind(void)
+{
+	_putchar('_');
+	_putchar('P');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
