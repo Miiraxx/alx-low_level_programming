2f90ac68d1583b9c54b0e8296731a4c3ac84a745
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-09-16 13:27:13 -0700

    7-print_diagonal.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers from 0 to 9.
+ * Return: returns nothing
+ */
+
+void print_numbers(void)
+{
+	int num;
+
+	for (num = '48'; num <= '58'; num++)
+	{
+		_putchar(num);
+	}
+	_putchar(10);
+}
---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers except 2 and 4
+ * Return: returns nothing
+ */
+
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = '48'; num <= 58; num++)
+	{
+		if ((n == 50) || (n == 52))
+		{
+			continue;
+		}
+		_putchar(num)
+	}
+	_putchar(10)
+}
