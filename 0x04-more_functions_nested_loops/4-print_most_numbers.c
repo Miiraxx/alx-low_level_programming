b73017299fda384ac8835470a85c1c9e5fc7eb9a
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-09-16 13:37:30 -0700

    7-print_diagonal.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,13 +10,11 @@ void print_most_numbers(void)
 {
 	int num;
 
-	for (num = '48'; num <= 58; num++)
+	for (num = 0; num <= 9; num++)
 	{
-		if ((n == 50) || (n == 52))
-		{
-			continue;
-		}
-		_putchar(num)
+		if (num != 2 && num != 4)
+			_putchar((num % 10) + '0');
 	}
-	_putchar(10)
+
+	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -19,9 +19,9 @@ void print_diagonal(int n)
 			{
 				_putchar(' ');
 			}
-
 			putchar('\\');
-			if (len == (n - 1))
+
+			if (len == n - 1)
 			{
 				continue;
 			}
