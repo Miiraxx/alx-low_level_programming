3dace67cd071d9d8c80c20f2d9b568d4ab2dee3b
Miracle Salami <salami.miracle.o@gmail.comcdsalami.miracle.o@gmailcd /alx-pre_course//alx-pre_course# git config --global user.name miracle>
2022-09-27 21:52:29 -0700

    memcpy

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,24 +1,21 @@
 #include "main.h"
 
 /**
- * _memcpy - Copies @n bytes from the memory area pointed
- * to by @src into that pointed to by @dest.
- * @dest: A pointer to the memory area to copy @src into.
- * @src: The source buffer to copy characters from.
- * @n: The number of bytes to copy from @src.
+ * _memcpy - copies n bytes from a memory area to another
+ * @dest: memory area to copy to
+ * @src: memory area to copy from
+ * @n: number of bytes to copy from beginning of src
  *
- * Return: A pointer to the destination buffer @dest.
+ * Return: dest
  */
 
-void *_memcpy(void *dest, const void *src, size_t n)
-
+char *_memcpy(char *dest, char *src, unsigned int n)
+	
 {
-	unsigned int i = 0;
+	unsigned int i;
 
-	for (; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
 
 	return (dest);
 }
